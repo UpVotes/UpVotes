c5aeb76f35b3308944a506607f0771f5ee9e5201
kumajaya <kumajaya@FNFILPT227>
2018-01-27 16:27:24 +0530

    Google search implemented

---
+++ UpVotes/Controllers/SearchController.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Mvc;
+
+
+namespace UpVotes.Controllers
+{
+    public class SearchController : Controller
+    {
+        public ActionResult CustomSearch(string Query)
+        {
+            ViewBag.Query = Query;
+            ViewBag.Title = "Search";
+            return View("~/Views/Search/Search.cshtml");
+            
+        }
+    }
+}
