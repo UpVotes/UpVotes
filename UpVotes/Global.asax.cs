d01494ddfcafc057111a6ca2b4d9cdb3afb2d84f
kumajaya <kumajaya@FNFILPT227>
2018-02-28 17:45:58 +0530

    Page Not Found Error Fix

--- UpVotes/Global.asax.cs
+++ UpVotes/Global.asax.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-
+using UpVotes.Controllers;
 namespace UpVotes
 {
     public class MvcApplication : System.Web.HttpApplication
@@ -17,5 +17,28 @@ namespace UpVotes
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-    }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            Server.ClearError();
+
+            RouteData routeData = new RouteData();
+            routeData.Values.Add("controller", "Error");
+            routeData.Values.Add("action", "PageNotFound");
+            routeData.Values.Add("exception", exception);
+
+            if (exception.GetType() == typeof(HttpException))
+            {
+                routeData.Values.Add("statusCode", ((HttpException)exception).GetHttpCode());
+            }
+            else
+            {
+                routeData.Values.Add("statusCode", 500);
+            }
+
+            IController controller = new ErrorController();
+            controller.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
+            Response.End();
+        }
+        }
 }
