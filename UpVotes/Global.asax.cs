95a5a5bc3125762661f162b9bbf33eeebd6ff0b9
kumajaya <kumajaya@FNFILPT227>
2018-10-22 17:46:13 +0530

    URL Redirection

--- UpVotes/Global.asax.cs
+++ UpVotes/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -40,5 +41,22 @@ namespace UpVotes
             controller.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
             Response.End();
         }
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            bool isGet = HttpContext.Current.Request.RequestType.ToLowerInvariant().Contains("get");
+            if (isGet && HttpContext.Current.Request.Url.AbsolutePath.Contains(".") == false)
+            {
+                string lowercaseURL = (Request.Url.Scheme + "://" + HttpContext.Current.Request.Url.Authority + HttpContext.Current.Request.Url.AbsolutePath);
+                if (Regex.IsMatch(lowercaseURL, @"[A-Z]"))
+                {
+                    lowercaseURL = lowercaseURL.ToLower() + HttpContext.Current.Request.Url.Query;
+
+                    Response.Clear();
+                    Response.Status = "301 Moved Permanently";
+                    Response.AddHeader("Location", lowercaseURL);
+                    Response.End();
+                }
+            }
         }
+    }
 }
